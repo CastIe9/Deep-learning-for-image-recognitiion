4e9c35e238a4042a28229c010c29ec544f946f51
CastIe9 <tjd028@gmail.com>
2024-06-21 12:49:14 +0900

    Create VGG.py

---
+++ VGG.py
@@ -0,0 +1,123 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+class ConvLayer(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ConvLayer, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.relu(x)
+        return x
+
+class VGG(nn.Module):
+    def __init__(self, features, num_classes=100):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            # nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            # nn.Dropout(0.5),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+def make_layers(cfg):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            layers += [ConvLayer(in_channels, v)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+cfg_vgg16 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+cfg_vgg19 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+
+vgg16 = VGG(make_layers(cfg_vgg16))
+vgg19 = VGG(make_layers(cfg_vgg19))
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)), # resize 수행
+    # data augmentation 수행
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
+
+trainloader = DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+testloader = DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
+
+model = vgg19  
+#model = vgg16
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+model_save_path = './vgg19_CIFAR100.pth'
+
+def train(model, trainloader, criterion, optimizer, epoch):
+    model.train()
+    running_loss = 0.0
+    for inputs, labels in tqdm(trainloader, desc=f'Epoch {epoch+1}', leave=False):
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    print(f'Epoch {epoch+1} - Training Loss: {running_loss/len(trainloader):.4f}')
+
+def test(model, testloader, criterion):
+    model.eval()
+    correct = 0
+    total = 0
+    test_loss = 0.0
+    with torch.no_grad():
+        for inputs, labels in testloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            test_loss += loss.item()
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy = 100 * correct / total
+    print(f'Test Loss: {test_loss/len(testloader):.4f}, Accuracy: {accuracy:.2f}%')
+
+num_epochs = 10
+
+if os.path.exists(model_save_path):
+    model.load_state_dict(torch.load(model_save_path))
+    print("Model loaded successfully.")
+
+for epoch in range(num_epochs):
+    train(model, trainloader, criterion, optimizer, epoch)
+    test(model, testloader, criterion)
+    torch.save(model.state_dict(), model_save_path)
+    print(f'Model parameters saved at epoch {epoch+1}')
